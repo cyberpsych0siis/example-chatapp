288c71d7a637b57ab69fe209dc8d41bc8014d536
Philipp Gaßner <activator112233@gmail.com>
2022-04-17 15:53:12 +0200

    Initial Commit

---
+++ Shared/TestApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp2App.swift
+//  Shared
+//
+//  Created by Philipp Gaßner on 17.04.22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
